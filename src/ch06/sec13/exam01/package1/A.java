44e96a101e47a5ef5656f98e091c25f0a0500fc1
unknown <aa25aa>
2025-04-22 16:20:16 +0900

    github 업로드연습

---
+++ src/ch06/sec13/exam01/package1/A.java
@@ -0,0 +1,18 @@
+
+package ch06.sec13.exam01.package1;
+
+public class A {
+
+	public A(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public A(boolean b) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public A(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/woori/BAM/Main.java
@@ -0,0 +1,25 @@
+package com.woori.BAM;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("====프로그램 시작====");
+		Scanner sc = new Scanner(System.in);//최적화 =>자원 1번 사용 개선
+		
+		while (true) { // 무한반복
+		    System.out.printf("cmd)");
+		    String cmd = sc.nextLine(); //cmd 변수를 만들어 낸 것은 재사용하기 위해서 cmd에는 문자값이 들어있는데
+			System.out.println("명령어)" +cmd);
+//			if (cmd.equals("exit")) {
+			if (cmd  == "exit") {
+				break;
+			}
+		}
+
+		sc.close();
+		System.out.println("====프로그램 종료====");
+
+	}
+}
