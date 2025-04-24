b768d213ff61222230a1f9a0ab78c07d06bf82bf
aa25aa <gm2520g@gmail.com>
2025-04-24 17:07:51 +0900

    Article 구현

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,25 +1,44 @@
 package com.woori.BAM;
-
 import java.util.Scanner;
-
 public class Main {
-	
 	public static void main(String[] args) {
-		System.out.println("====프로그램 시작====");
-		Scanner sc = new Scanner(System.in);//최적화 =>자원 1번 사용 개선
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in); 
+       
+		int id = 1;
 		
-		while (true) { // 무한반복
-		    System.out.printf("cmd)");
-		    String cmd = sc.nextLine(); //cmd 변수를 만들어 낸 것은 재사용하기 위해서 cmd에는 문자값이 들어있는데
-			System.out.println("명령어)" +cmd);
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine().trim(); 
+			
+			
 			if (cmd.equals("exit")) {
-//			if (cmd  == "exit") {
 				break;
+			} 
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해 주세요");
+				continue;
+			}
+			
+			if (cmd.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else if(cmd.equals("article write")){
+				
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine().trim(); 
+				System.out.printf("내용 : ");
+				String body = sc.nextLine().trim(); 
+				System.out.println(id +"번글이 생성되었습니다");
+				id++;
+				
+			}else {
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
-
+		
 		sc.close();
-		System.out.println("====프로그램 종료====");
-
+		System.out.println("== 프로그램 종료 ==");
 	}
+
 }
---
+++ src/com/woori/BAM/Main4.java
@@ -0,0 +1,11 @@
+package com.woori.BAM;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner(System in);
+		final int STUDENT_COUNT = 5;
+		int
+		
+}
+}
